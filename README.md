9472f9e0c565b044b8b660d93a4592792ec65a83
yuichi.watanabe <yuichi.watanabe@nulab-inc.com>
2017-08-26 22:28:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# lsf-notifier
+
+## Bugs and Feedback
+For bugs, questions and discussions please use the Github Issues.
+
+## License
+[MIT License](http://www.opensource.org/licenses/mit-license.php)
---
+++ lsf-notifier/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"flag"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/garyburd/redigo/redis"
+	"github.com/nulab/go-typetalk/typetalk"
+)
+
+type Config struct {
+	HouseIds        []string `json:"house_ids"`
+	TypetalkTopicId int      `json:"typetalk_topic_id"`
+	TypetalkToken   string   `json:"typetalk_token"`
+	RedisNetWork    string   `json:"redis_network"`
+	RedisPort       string   `json:"redis_port"`
+}
+
+func main() {
+
+	// ---------------------------------------
+	// parse command line args
+	// ---------------------------------------
+	executablePath, err := os.Executable()
+	if err != nil {
+		fmt.Println("os.Executable error: " + err.Error())
+		return
+	}
+	defaultConfFilePath := executablePath + "/conf.json"
+	var configFilePath string
+	flag.StringVar(&configFilePath, "config", defaultConfFilePath, "config file path")
+	flag.StringVar(&configFilePath, "c", defaultConfFilePath, "config file path")
+	flag.Parse()
+
+	// ---------------------------------------
+	// parse config
+	// ---------------------------------------
+	config, err := parseConfig(configFilePath)
+	if err != nil {
+		fmt.Println("parse config error: " + err.Error())
+		return
+	}
+
+	// ---------------------------------------
+	// setup redis
+	// ---------------------------------------
+	c, err := redis.Dial(config.RedisNetWork, config.RedisPort)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer c.Close()
+
+	// ---------------------------------------
+	// setup typetalk
+	// ---------------------------------------
+	client := typetalk.NewClient(nil).SetTypetalkToken(config.TypetalkToken)
+
+	// ---------------------------------------
+	// main
+	// ---------------------------------------
+	for _, houseId := range config.HouseIds {
+
+		houseUrl := "https://lsf.jp/rent/bui_1.php?dn=" + houseId
+		doc, err := goquery.NewDocument(houseUrl)
+		if err != nil {
+			fmt.Print("Scraping failed. House ID: " + houseId)
+		}
+
+		houseName := doc.Find("table.jyu_table tr td span").First().Text()
+		hitNumBox := doc.Find("#hitnum_box")
+
+		currentHitNumBoxText := hitNumBox.Text()
+
+		hitNumBoxTextKey := "HitNumBoxText-" + houseId
+		beforeHitNumBoxText, err := redis.String(c.Do("GET", hitNumBoxTextKey))
+		if err != nil {
+			fmt.Println("Before house data does not exist. House name: " + houseName)
+		}
+
+		if currentHitNumBoxText == beforeHitNumBoxText {
+			fmt.Println("It's the same as the before state. House name: " + houseName)
+			continue
+		}
+
+		c.Do("SET", hitNumBoxTextKey, currentHitNumBoxText)
+
+		message := "House name: " + houseName + "\n"
+		hitNumBox.Find("span").Each(func(i int, s *goquery.Selection) {
+			if i == 0 {
+				message += "Repaired house: " + s.Text() + "\n"
+			} else if i == 1 {
+				message += "General houes: " + s.Text() + "\n"
+			}
+		})
+		message += "URL: " + houseUrl + "\n"
+		ctx := context.Background()
+		client.Messages.PostMessage(ctx, config.TypetalkTopicId, message, nil)
+		fmt.Println(message)
+	}
+
+}
+
+func parseConfig(path string) (Config, error) {
+	var c Config
+	jsonString, err := ioutil.ReadFile(path)
+	if err != nil {
+		return c, err
+	}
+	err = json.Unmarshal(jsonString, &c)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
+}
